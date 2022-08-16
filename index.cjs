3be8e8aa86554b328a5e0da1275e61f806559d8f
Jonas Abrahams <jonas.abrahams@farben-profi.de>
2022-08-16 11:19:03 +0200

    initial commit

---
+++ index.cjs
@@ -0,0 +1,54 @@
+const { app, BrowserWindow } = require('electron')
+//const path = require('path')
+const serve = require('electron-serve')
+const loadURL = serve({ directory: 'dist'})
+
+let mainWindow;
+
+function isDev() {
+    return !app.isPackaged
+}
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 900,
+        height: 620,
+        webPreferences: {
+            nodeIntegration: true,
+            //preload: path.join(__dirname, 'preload.js')
+        },
+        //icon: path.join(__dirname, 'public/favicon.png'),
+        show: false
+    })
+
+    if (isDev()) {
+        loadPage()
+    } else {
+        loadURL(mainWindow)
+    }
+
+    mainWindow.on('closed', () => {
+        mainWindow = null
+    })
+
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show()
+    })
+}
+
+function loadPage() {
+    console.log('loading')
+    mainWindow.loadURL('http://localhost:5173/').catch((err) => {
+        setTimeout(() => { loadPage() }, 200)
+    })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+    if (mainWindow === null) createWindow()
+})
