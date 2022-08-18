f9e0626ad6d51e35c33aa1541e0d070b1c7d54b8
Jonas Abrahams <jonas.abrahams@farben-profi.de>
2022-08-18 12:17:44 +0200

    remove App.spec.svelte

--- src/App.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect, it } from 'vitest'
-import { render } from '@testing-library/svelte'
-import App from './App.svelte'
-
-it('is empty', () => {
-    const result = render(App)
-
-    expect(result).toBeTruthy()
-})
