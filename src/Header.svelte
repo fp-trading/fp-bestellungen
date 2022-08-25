2b4dafa5f16e07ed0c9a5cfa14e923cc19f51e0c
Jonas Abrahams <jonas.abrahams@farben-profi.de>
2022-08-25 13:35:54 +0200

    connect parser to ui and get clipboard text

--- src/Header.svelte
+++ src/Header.svelte
@@ -5,10 +5,28 @@
     HeaderUtilities,
   } from "carbon-components-svelte";
   import { DocumentImport } from "carbon-icons-svelte";
+  import EmailParser from "./lib/EmailParser";
+
+  async function handleImportClick() {
+    const clipboardContent = await navigator.clipboard.readText();
+    parseClipboardIfContainsEmail(clipboardContent);
+  }
+
+  function parseClipboardIfContainsEmail(clipboardContent: string) {
+    try {
+      new EmailParser().parse(clipboardContent);
+    } catch (err) {
+      handleParserError(err);
+    }
+  }
+
+  function handleParserError(error: Error) {
+    console.log(error.message);
+  }
 </script>
 
 <Header company="Farben-Profi" platformName="Bestellungen">
   <HeaderUtilities>
-    <HeaderGlobalAction icon={DocumentImport} />
+    <HeaderGlobalAction icon={DocumentImport} on:click={handleImportClick} />
   </HeaderUtilities>
 </Header>
