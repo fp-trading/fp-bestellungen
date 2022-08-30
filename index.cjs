e0f042f2cd4e3356a60de0c354cad84375dc3262
Jonas Abrahams <jonas.abrahams@farben-profi.de>
2022-08-30 10:11:21 +0200

    basic express server running for IPC

--- index.cjs
+++ index.cjs
@@ -1,6 +1,8 @@
+const bodyParser = require('body-parser');
 const { app, BrowserWindow } = require('electron')
 //const path = require('path')
 const serve = require('electron-serve')
+const express = require('express')
 const loadURL = serve({ directory: 'dist'})
 
 let mainWindow;
@@ -51,4 +53,22 @@ app.on('window-all-closed', () => {
 
 app.on('activate', () => {
     if (mainWindow === null) createWindow()
+})
+
+const expressApp = express()
+
+expressApp.use(bodyParser.json())
+
+expressApp.post('/api/fulfill', async (req, res) => {
+    console.log(req.body)
+    res.status(200).send('fulfilling')
+})
+
+expressApp.all('*', async (req, res) => {
+    console.log(req.body)
+    res.status(404).json( { error: 'invalid endpoint' })
+})
+
+expressApp.listen(5172, () => {
+    console.log('Express Server running and listening on port 5172...')
 })
